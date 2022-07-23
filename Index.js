f4505440f442de651b706b922d139ff3987457fe
Arleys Gatica <gatica.arleys16@gmail.com>
2022-07-22 20:02:33 -0600

    Starting with the CRUD-faker on the Router with its route and running on the exposed port

---
+++ Index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const routerApi = require('./Router');
+const app = express();
+const port = 3000;
+
+//Middleware for create a new product 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+ routerApi(app);
+
+app.listen(port, () => { console.log(`Example app listening on port ${port}!`) });
+
+
+
---
+++ Router/Index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const productsRouter = require('./Product');
+
+function routerApi(app) {
+    const router = express.Router();
+    app.use('/api/v1', router);
+    router.use('/products', productsRouter); 
+}
+
+module.exports = routerApi;
