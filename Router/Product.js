9a7d58b9f9104ccf246d3f1fbe518208459179ec
Arleys Gatica <gatica.arleys16@gmail.com>
2022-07-22 20:28:39 -0600

    Implementing a clean architecture. And creating a service with a business logic. Small example

--- Router/Product.js
+++ Router/Product.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const faker = require('faker');
+
+const ProductsService = require('./../services/');
 
 const router = express.Router();
+const service = new ProductsService();
 
 router.get('/', (req, res) => {
-    const products = [];
-    const { size } = req.query;
-    const limit = size || 10;
-    for (let index = 0; index < limit; index++) {
-        products.push({
-            name: faker.commerce.productName(),
-            price: parseInt(faker.commerce.price(), 10),
-            image: faker.image.imageUrl(),
-        });
-    }
+    const products = service.find();
     res.json(products);
 });
 
@@ -23,16 +16,13 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
     const { id } = req.params;
-    res.json({
-        id,
-        name: 'Product 2',
-        price: 2000
-    });
+    const product = service.findOne(id);
+    res.json(product);
 });
 
 router.post('/', (req, res) => {
     const body = req.body;
-    res.json({
+    res.status(201).json({
         message: 'created',
         data: body
     });
