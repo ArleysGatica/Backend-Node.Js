4fbe2bce4cba2315cfad3354a67f90a25a3ed53b
Arleys Gatica <gatica.arleys16@gmail.com>
2022-08-09 10:18:45 -0600

    leaving everything as it was

--- Libs/Sequile.js
+++ Libs/Sequile.js
@@ -8,7 +8,7 @@ const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-    dialect: 'postgres',
+    dialect:'postgres',
     logging: true,
 });
 
